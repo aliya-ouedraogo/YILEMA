193ae6f2f3fd242cbcdff0a4ccb799e1b81bee3f
Michael SACKO <michaelsacko04gmail.com>
2026-07-22 19:40:37 +0000

    Gere le cas urlVideo Vide dans le lecteur Video

--- lib/features/catalogue/screens/video_player_screen.dart
+++ lib/features/catalogue/screens/video_player_screen.dart
@@ -12,16 +12,23 @@ class VideoPlayerScreen extends StatefulWidget {
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  late VideoPlayerController _controller;
+  VideoPlayerController? _controller;
   bool _hasError = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.content.urlVideo ?? ''))
+
+    final url = widget.content.urlVideo;
+    if (url == null || url.isEmpty) {
+      _hasError = true;
+      return;
+    }
+
+    _controller = VideoPlayerController.networkUrl(Uri.parse(url))
       ..initialize().then((_) {
         setState(() {});
-        _controller.play();
+        _controller!.play();
       }).catchError((_) {
         setState(() => _hasError = true);
       });
@@ -29,7 +36,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 
@@ -45,31 +52,31 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
       body: Center(
         child: _hasError
             ? const Text(
-                'Impossible de lire cette video',
+                'Vidéo non disponible pour ce contenu',
                 style: TextStyle(color: Colors.white),
               )
-            : _controller.value.isInitialized
+            : (_controller != null && _controller!.value.isInitialized)
                 ? AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
+                    aspectRatio: _controller!.value.aspectRatio,
                     child: Stack(
                       alignment: Alignment.bottomCenter,
                       children: [
-                        VideoPlayer(_controller),
-                        VideoProgressIndicator(_controller, allowScrubbing: true),
+                        VideoPlayer(_controller!),
+                        VideoProgressIndicator(_controller!, allowScrubbing: true),
                       ],
                     ),
                   )
                 : const CircularProgressIndicator(),
       ),
-      floatingActionButton: _controller.value.isInitialized
+      floatingActionButton: (_controller != null && _controller!.value.isInitialized)
           ? FloatingActionButton(
               onPressed: () {
                 setState(() {
-                  _controller.value.isPlaying ? _controller.pause() : _controller.play();
+                  _controller!.value.isPlaying ? _controller!.pause() : _controller!.play();
                 });
               },
               child: Icon(
-                _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                _controller!.value.isPlaying ? Icons.pause : Icons.play_arrow,
               ),
             )
           : null,
